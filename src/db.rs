b8797e5e387008c6fd158c1fe3c37716af14a91d
JRBros2346 <jrbros2346@gmail.com>
2024-08-14 09:07:37 +0530

    [ADD] Fixes from Previous Mistakes 🔄️

--- src/db.rs
+++ src/db.rs
@@ -1,37 +1,54 @@
-use surrealdb::{engine::remote::ws::Ws, Surreal};
+use surrealdb::{engine::remote::ws::{Client, Ws}, Connect, Surreal};
 
-use crate::types::Money;
-
-pub struct Database(Surreal<Ws>);
+pub struct Database(Connect<Client, Surreal<Client>>);
 impl Database {
-    pub fn open() -> Result<Self> {
-        let mut db = SledStorage::new(DB_PATH.to_str().expect("Invalid Path to DB..!"))
-            .map(Glue::new)
-            .map(Self)?;
-        // futures::executor::block_on(db.create_companies())?;
-        // futures::executor::block_on(db.create_projects())?;
-        // futures::executor::block_on(db.create_employees())?;
-        // futures::executor::block_on(db.create_salaries())?;
-        // futures::executor::block_on(db.create_worked())?;
-        // futures::executor::block_on(db.create_payment())?;
-        // futures::executor::block_on(db.create_tools())?;
-        // futures::executor::block_on(db.create_tool_costs())?;
-        // futures::executor::block_on(db.create_tools_tracking())?;
-        // futures::executor::block_on(db.create_expenses())?;
+    pub fn open() -> Result<Self, Error> {
+        let db = Self(Surreal::new::<Ws>(format!(
+            "{}:{}",
+            dotenv::var("DB_HOST")?,
+            dotenv::var("DB_PORT")?,
+        )));
         Ok(db)
     }
-    pub fn add_company(&mut self, name: &str) -> Result<()> {
-        futures::executor::block_on(self.insert_company(name))
+    // pub fn add_company(&mut self, name: &str) -> Result<()> {
+    //     futures::executor::block_on(self.insert_company(name))
+    // }
+    // pub fn get_companies(&mut self) -> Result<Vec<String>> {
+    //     futures::executor::block_on(self.select_companies())
+    // }
+    // pub fn add_project(
+    //     &mut self,
+    //     name: &str,
+    //     company: &str,
+    //     estimate: Option<Money>,
+    // ) -> Result<()> {
+    //     futures::executor::block_on(self.insert_project(name, company, estimate))
+    // }
+}
+
+#[derive(Debug)]
+pub enum Error {
+    Db(surrealdb::Error),
+    Env(dotenv::Error),
+}
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Db(e) => write!(f, "{e}"),
+            Self::Env(e) => write!(f, "{e}"),
+        }
     }
-    pub fn get_companies(&mut self) -> Result<Vec<String>> {
-        futures::executor::block_on(self.select_companies())
+}
+impl From<surrealdb::Error> for Error {
+    fn from(value: surrealdb::Error) -> Self {
+        Self::Db(value)
     }
-    pub fn add_project(
-        &mut self,
-        name: &str,
-        company: &str,
-        estimate: Option<Money>,
-    ) -> Result<()> {
-        futures::executor::block_on(self.insert_project(name, company, estimate))
+}
+impl From<dotenv::Error> for Error {
+    fn from(value: dotenv::Error) -> Self {
+        Self::Env(value)
     }
+}
+impl std::error::Error for Error {
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use view::*;
 use xilem::*;
 
 mod db;
-mod types;
 
 struct State {
     db: Database,
@@ -11,7 +10,7 @@ struct State {
 
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let state = State {
-        db: Database::open(),
+        db: Database::open()?,
     };
     let app = Xilem::new(state, app_logic);
     app.run_windowed(EventLoop::with_user_event(), "Malta".to_string())?;
